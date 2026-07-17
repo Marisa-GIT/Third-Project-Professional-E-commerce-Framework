ea59d60098051fbb8b8e21064aa0481444eb5a2d
isabelv123 <isabelvides1@outlook.com>
2026-07-17 15:57:55 -0500

    refactor: simplify test data manager

--- utils/test_data_manager.py
+++ utils/test_data_manager.py
@@ -11,3 +11,22 @@ class TestDataManager:
     @staticmethod
     def get_user(user_type):
         return TestDataManager.read_users()[user_type]
+
+    @staticmethod
+    def get_product(product_key):
+
+        with open("data/products.json", "r") as file:
+            productos = json.load(file)
+
+            if product_key in productos:
+                return productos[product_key]
+            
+        raise KeyError(f"La clave '{product_key}' no existe en products.json")
+
+    @staticmethod
+    def get_specific_products(clavelist):
+
+        with open("data/products.json", "r") as file:
+            productos = json.load(file)
+       
+            return [productos[clave] for clave in clavelist if clave in productos]
