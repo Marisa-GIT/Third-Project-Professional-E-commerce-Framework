e0382d32e709e9a3ac0a7f7a275497f1458b0f1e
isabelv123 <isabelvides1@outlook.com>
2026-07-21 09:45:17 -0500

    feat: add purchase flow after removing products

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -66,7 +66,32 @@ class TestCart:
         assert success_message == "Thank you for your order!", \
             f"La compra falló o el mensaje es incorrecto. Mensaje obtenido: '{success_message}'"
 
-   
+    def test_purchase_after_remove_product(self, login):
+        inventory_page = login()
+
+        purchase_two_products = TestDataManager.get_specific_products(
+            ["fleece_jacket", "bolt_tshirt"]
+        )
+
+        for product in purchase_two_products:
+            inventory_page.add_product_to_cart(product["name"])
+
+        cart_page = inventory_page.open_cart()
+
+        product = TestDataManager.get_product("fleece_jacket")
+
+        cart_page.remove_product(product["name"])
+
+        checkout_info_page = cart_page.checkout()
+
+        checkout_info_page.complete_information("Juan", "Pérez", "110111")
+        checkout_overview_page = checkout_info_page.submit_information()
+
+        checkout_complete_page = checkout_overview_page.finish()
+
+        success_message = checkout_complete_page.get_complete_header()
+        assert success_message == "Thank you for your order!", \
+            f"La compra falló o el mensaje es incorrecto. Mensaje obtenido: '{success_message}'"
 
 
 
